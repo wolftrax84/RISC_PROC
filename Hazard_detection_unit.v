75c7f300db9d3bfb67a2283fe10b0bacee372a16
teh117 <teh117@msstate.edu>
2011-04-08 15:38:01 -0500

    Revamped

--- Hazard_detection_unit.v
+++ Hazard_detection_unit.v
@@ -18,15 +18,19 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module Hazard_detection_unit(Rs, Rt, Ex_rt, Ex_memread, Mem_memread, Mem_rt, branch, ctrl_flush, PCwrite, Id_write);
+module Hazard_detection_unit(Rs, Rt, Ex_rt, Ex_memread, Mem_memread, Mem_rt, Wb_rt, Wb_memread,brlegt, breq, 
+										ctrl_flush, PCwrite, Id_write);
 
 //Inputs
 input [2:0] Rs, Rt;
 input [2:0] Ex_rt;
 input [2:0] Mem_rt;
+input [2:0] Wb_rt;
 input Ex_memread;
 input Mem_memread;
-input branch;
+input Wb_memread;
+input brlegt;
+input breq;
 
 
 
@@ -47,11 +51,21 @@ always@(*)
 					end
 			end
 			
-		if(Mem_memread == 1 && branch)
+		if(Mem_memread == 1 && brlegt)
 			begin
 				if((Mem_rt == Rs) || (Mem_rt == Rt))
 					begin
-						ctrl_flsh = 1;
+						ctrl_flush = 1;
+						PCwrite = 0;
+						Id_write = 0;
+					end
+			end
+			
+		if(Wb_memread == 1 && breq)
+			begin
+				if((Wb_rt == Rs) || (Wb_rt == Rt))
+					begin
+						ctrl_flush = 1;
 						PCwrite = 0;
 						Id_write = 0;
 					end
